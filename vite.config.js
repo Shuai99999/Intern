78863e32af378895095078e524649130d8ad3e60
Shuai99999 <gaoshuai099@gmail.com>
2026-02-11 20:25:21 -0700

    1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/intern/",
+  base: "https://shuai99999.github.io/intern/",
   plugins: [react()],
   resolve: {
     alias: {
